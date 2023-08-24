5356765319fe66718292493123600de961f6cc4e
IsaacNtakirutimana <isaacntakirutimana12@gmail.com>
2023-08-24 12:34:27 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+*_strcmp - function that compares two strings
+*@s1: destination string
+*@s2: source string
+*Return: return i
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+int i;
+
+i = strcmp(s1, s2);
+
+return (i);
+}
