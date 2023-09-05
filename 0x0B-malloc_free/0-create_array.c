edf3e36311fddcc245ef12bdf19270cd7419ded3
IsaacNtakirutimana <isaacntakirutimana12@gmail.com>
2023-09-05 12:56:52 -0700

    free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,14 +11,14 @@
 char *create_array(unsigned int size, char c)
 {
 char *a;
-unsigned int x;
+unsigned int i;
 if (size == 0)
 return (NULL);
 a = malloc(size * sizeof(*a));
 if (a == NULL)
 return (NULL);
-for (x = 0; x < size; x++)
-a[x] = c;
+for (i = 0; i < size; i++)
+a[i] = c;
 
 return (a);
 }
