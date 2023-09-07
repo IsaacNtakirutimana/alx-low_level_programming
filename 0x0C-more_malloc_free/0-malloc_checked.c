3c79c312405b844e74765d8f7a8dcfe934e00097
IsaacNtakirutimana <isaacntakirutimana12@gmail.com>
2023-09-07 12:24:13 -0700

    Trust

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - allocate memory with malloc
+ * @b: unsigned int type
+ * Return: return pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+int *isaac;
+isaac= malloc(b);
+if (isaac == NULL)
+exit(98);
+
+return (isaac);
+}
