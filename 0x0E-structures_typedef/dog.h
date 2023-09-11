94a9822e854f81463e68eab96cac194846093f71
IsaacNtakirutimana <isaacntakirutimana12@gmail.com>
2023-09-11 10:30:30 -0700

    goggy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,23 +2,22 @@
 #define DOG_H
 
 /**
- *  * struct dog - gives dog's info
- *   * @name: name of the dog?
- *    * @age: age of the dog?
- *     * @owner: who owns the dog?
- *      *
- *       * Description: descripbes the dog
- *        */
+ * struct dog - gives dog's info
+ * @name: name of the dog?
+ * @age: age of the dog?
+ * @owner: who owns the dog?
+ * Description: descripbes the dog
+ */
 struct dog
 {
-		char *name;
-			float age;
-				char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 /**
- *  * dog_t - typedef for struct dog
- *   */
+ * dog_t - typedef for struct dog
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
